54a30a4f684271c5fbd9bea107903a9d59bdc1cd
Lydia-BEDRI <bedrilydia118@gmail.com>
2025-05-29 15:58:05 +0200

    feat: add CuisineAlgerienne component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,12 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import FAQ from '../components/Faq.vue'
 import MentionsLegales from '@/components/MentionsLegales.vue'
+import CuisineAlgerienne from '@/components/CuisineAlgerienne.vue'
 
 
 const routes = [
   { path: '/', name: 'Accueil', component: Home },
   { path: '/faq', name: 'FAQ', component: FAQ },
   { path:'/mentions-legales', name: 'Mentions légales', component: MentionsLegales },
+  { path:'/cuisine-algerienne', name: 'Cuisine algérienne', component: CuisineAlgerienne },
 ]
 
 const router = createRouter({
